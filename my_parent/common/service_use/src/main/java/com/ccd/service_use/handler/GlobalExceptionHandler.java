6b5b16ec185dffcbc7be872b0e5b42ca8c106c48
CCD <CCD@ikun.com>
2023-04-02 14:49:19 +0800

    测试提交

--- my_parent/common/service_use/src/main/java/com/ccd/service_use/handler/GlobalExceptionHandler.java
+++ my_parent/common/service_use/src/main/java/com/ccd/service_use/handler/GlobalExceptionHandler.java
@@ -19,7 +19,6 @@ public class GlobalExceptionHandler {
     public R error(Exception e) {
         log.error("执行了全局异常处理");
         e.printStackTrace();
-        System.out.println("555");
         return R.error().message("执行了全局异常处理..");
     }
 
